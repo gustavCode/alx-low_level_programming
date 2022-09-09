922886bf0749d17bef4d6b2a2178012af8b3c288
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-09 04:16:29 -0700

    Adding the file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		putchar((c % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
