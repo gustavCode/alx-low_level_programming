7fe207d2f8a888a3eb6c8b285f868a635c4963c9
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-09 07:10:35 -0700

    Adding the file 8-print_base16.c

---
+++ 0x01-variables_if_else_while8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numeral;
+	char letter;
+
+	for (numeral = 0; numeral < 10; numeral++)
+		putchar((numberal % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar(letter);
+
+	return (0);
+}
