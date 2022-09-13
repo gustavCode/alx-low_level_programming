f2183b8cbed2685dd692517ab0519341c9a6318e
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-13 06:38:54 -0700

    Adding the file 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ *
+ * Return: returns void
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+}
