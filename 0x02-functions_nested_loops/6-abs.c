ad27cb83ea2875480273fb39ec8f4545f0d38174
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-13 08:13:47 -0700

    Adding the file 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abolute value of an integer
+ * @a: the integer to be checked
+ *
+ * Return: absolute value of n
+ */
+int _abs(int a)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
