1948d2c6e7f8d066ffbbaa80385fa83c9327c044
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-13 06:18:09 -0700

    Adding the main.h header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ *
+ * Purpose: Contains function prototypes used in the
+ * 	0x02-functions_nested_loops
+ */
+
+int _putchar(char c);
+
+#endif
