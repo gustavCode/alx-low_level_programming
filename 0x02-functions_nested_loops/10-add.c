08ab3b61d3fe8bfdb5acbc5fcdd465b79cb77a4b
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-13 13:34:29 -0700

    Adding the file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer for addition
+ * @b: second integer for addition
+ *
+ * Return: returns the result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
