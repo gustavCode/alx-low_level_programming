4bd2d1e5a080c947778edb37d186ce4572b5f056
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-15 13:39:51 -0700

    Updating the file 10-print_triangles.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,22 +11,23 @@ void print_triangle(int size)
 	int i, j, k;
 
 	if (size <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j = (size - 1); j >= 0; j--)
+			for (j = (size - i); j > 1; j--)
 			{
-				if (j <= i)
-				{
-					_putchar('#');
-				}
-				else
-				{
-					_putchar(' ');
-				}
+				_putchar(32);
 			}
+
+			for (k = 0; k <= i; k++)
+			{
+				_putchar(35);
+			}
+
 			_putchar('\n');
 		}
 	}
