62af66d2026fe4ea31ac3cea45443468d251dcf4
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-15 18:15:48 -0700

    Adding the file 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the largest prime factor of the
+ * number 612852475143
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long prime  = 612852475143;
+	long divident;
+
+	while (divident < (prime / 2))
+	{
+		if ((prime % 2) == 0)
+		{
+			prime /= 2;
+			continue;
+		}
+		
+		for (divident = 3; divident < (prime / 2); divident += 2)
+		{
+			if ((prime % divident) == 0)
+				prime /= divident;
+		}
+	}
+
+	printf("%ld\n", prime);
+
+	return (0);
+}
+	
