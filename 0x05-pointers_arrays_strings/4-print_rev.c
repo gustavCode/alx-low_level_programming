880a7e88716acf4bbec328b21a9de076f939b706
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-19 20:41:12 -0700

    Updating 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,12 @@
  */
 void print_rev(char *s)
 {
-	int l = 0, i;
+	int i = 0;
 
-	while (s[i++])
-		l++;
+	while (s[i] != '\0')
+		i++;
 
-	for (i = l - 1; i >= 0; i--)
+	for (i -= 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
