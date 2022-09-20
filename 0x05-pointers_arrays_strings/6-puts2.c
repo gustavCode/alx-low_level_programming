d0aa9603edcb3a945a916ece82acaf4d3392f09b
gustavCode <87843734+gustavCode@users.noreply.github.com>
2022-09-20 04:35:04 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,12 @@
  */
 void puts2(char *str)
 {
-	int i, len = 0;
-
-	while (str[i++])
-		len++;
-
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
-
-	_putchar('\n');
+	if (str)
+	{
+		do {
+			if (*str)
+				_putchar(*str++);
+		} while (*str++);
+		_putchar('\n');
+	}
 }
