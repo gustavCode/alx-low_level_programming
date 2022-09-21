52a031955e884c05f49a13b5305971a38412971d
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-21 11:24:24 -0700

    Adding 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase strings to uppercase
+ * @s: string to be modified
+ *
+ * Return: modified string
+ */
+char *string_toupper(char *s)
+{
+	int x = 0;
+
+	while (s[x])
+	{
+		if (s[x] >= 97 && s[x] <= 122)
+		{
+			s[x] -= 32;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
