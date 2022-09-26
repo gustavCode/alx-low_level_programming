80320725af1fc62e655314f15153d9b61ed765be
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-26 11:37:45 -0700

    Updating 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strchr - locate character in string
+ * _strchr - Locate occurance of first character in string
  * @s: source string
  * @c: character to find
- * 
+ *
  * Return: the string from character found
  */
 char *_strchr(char *s, char c)
@@ -12,9 +12,7 @@ char *_strchr(char *s, char c)
 	int x = 0, y;
 
 	while (s[x])
-	{
 		x++;
-	}
 
 	for (y = 0; y <= x; y++)
 	{
