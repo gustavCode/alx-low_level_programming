3f1a6234c643a9476b31ec822f4c706d3f08447c
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-28 09:38:57 -0700

    Adding 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String to reverse
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
