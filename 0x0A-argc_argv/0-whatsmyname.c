9bc9593f5caea88566b088318dcd59b83aed5654
Gustav Ofosu <gustavofosu@gmail.com>
2022-09-30 09:33:40 -0700

    Adding 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print the programs name, followed by newline
+ * @argc: count of arguments
+ * @argv: pointer to and array of strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
