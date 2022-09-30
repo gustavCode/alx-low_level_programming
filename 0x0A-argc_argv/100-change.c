9d9362d332276f8e51b272229dca35bfd16f63a0
gustavCode <87843734+gustavCode@users.noreply.github.com>
2022-09-30 18:56:42 +0000

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -35,13 +35,11 @@ int main(int argc, char *argv[])
 			cents -= 10;
 			continue;
 		}
-
 		if ((cents - 5) >= 0)
 		{
 			cents -= 5;
 			continue;
 		}
-
 		if ((cents - 2) >= 0)
 		{
 			cents -= 2;
