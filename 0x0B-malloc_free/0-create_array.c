989b10b6a24d43151a086263bbfdc3f8d69b6775
Gustav Ofosu <gustavofosu@gmail.com>
2022-10-04 00:50:52 -0700

    Updating 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_arrray(unsigned int size, char c)
 	char *arr;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	arr = malloc(size * sizeof(char));
 
-	if (size == NULL)
-		return NULL;
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		arr[i] = c;
