b840d5eecceaf11191baca5377720cff146295ee
Gustav Ofosu <gustavofosu@gmail.com>
2022-10-10 15:57:58 -0700

    Adding dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This contains source files for structures and typdef projects
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog attributes
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: Atrributes of a dog
+ */
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
