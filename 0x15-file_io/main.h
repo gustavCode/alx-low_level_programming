b3800f8959100f9ea8c1a01154ad085271099ede
Gustav Ofosu <gustavofosu@gmail.com>
2022-10-31 11:39:56 -0700

    Updating 3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,5 +11,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
+void copy_file(const char *src, const char *dest);
 
 #endif
